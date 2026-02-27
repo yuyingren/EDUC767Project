3d2dfa282d5c2968666644eee683d684edbc316e
Yuying Ren <55150457+yuyingren@users.noreply.github.com>
2026-02-27 12:31:37 -0500

    Add files via upload

---
+++ pools/3719/3719.js
@@ -0,0 +1,17 @@
+window.CPPool3719Init = function() { cp.model['3719Data']={
+};cp.poolSlideResources['pool3719']={
+};
+cp.model.data['3719']={
+pqs:[],
+gqs:[],
+sqs:[],
+rgqs:[],
+rsqs:[],
+hasCC:true
+};
+cp.poolResources["3719Images"]=[];
+cp.poolResources["3719Videos"]=[
+];
+cp.poolResources["3719SlideVideos"]=[
+];
+}
